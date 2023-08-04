a92a233074287a93a2549a84522dc4c0ef9674dc
Ikanor1221 <ilya.bochkov97@gmail.com>
2023-08-04 20:51:23 +0300

    Implement basic object generation

--- src/model.js
+++ src/model.js
@@ -1,8 +1,25 @@
 function createModel() {
-  // const apiKey =
-  //   "http://api.weatherapi.com/v1/forecast.json?key=dd718f92e0a245e2932214841232007&q=London&days=3&aqi=no&alerts=no";
+  const currentLocationWeather = [];
 
-  // method below requires error catching and dynamic key
+  function createWeatherDay(
+    name,
+    date,
+    icon,
+    weather,
+    temperature,
+    humidity,
+    wind
+  ) {
+    return {
+      name,
+      date,
+      icon,
+      weather,
+      temperature,
+      humidity,
+      wind,
+    };
+  }
 
   async function callApi(location) {
     //Receive and return API data
@@ -17,15 +34,21 @@ function createModel() {
     return weatherData;
   }
 
-  function formObjects(weatherData) {
-    console.log(weatherData.forecast.forecastday[0]);
-  }
-
   async function retreiveData(location = "London") {
     const weatherData = await callApi(location);
     formObjects(weatherData);
 
     return;
+  }
+
+  function formObjects(weatherData) {
+    console.log(weatherData.location.name);
+    console.log(weatherData.forecast.forecastday[0].date);
+    console.log(weatherData.forecast.forecastday[0].day.condition.icon);
+
+    // let name;
+    // date, icon, weather, temperature, humidity, wind
+    // createWeatherDay(name, date, icon, weather, temperature, humidity, wind);
   }
 
   // function parseData
